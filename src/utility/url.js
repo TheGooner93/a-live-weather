af4916da589370fed7c01db82f3ba72c7b152119
Mohammed Ehab <mohammedehab93@gmail.com>
2024-04-28 09:16:18 +0400

    Migrate to OneCall API 3.0

--- src/utility/url.js
+++ src/utility/url.js
@@ -1,6 +1,6 @@
 export const getWeatherInfoUrl = (lat, lon, excludeArray, unitsSystem) => {
     const excludeString = excludeArray.join(',');
-    return `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${excludeString}&units=${unitsSystem}&appid=${process.env.VUE_APP_OPENWEATHER_API_KEY}`;
+    return `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${excludeString}&units=${unitsSystem}&appid=${process.env.VUE_APP_OPENWEATHER_API_KEY}`;
 }
 
 export const getGeocodingUrl = (locationString) => {
